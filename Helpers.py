3590bd0f79fdc73867a03b9d5860c9dcbb18a003
Hamed Mahmoudkhani <ainyava@gmail.com>
2021-09-08 14:53:35 +0430

    Initial Commit

---
+++ Helpers.py
@@ -0,0 +1,19 @@
+class Offset:
+	dw_client_state = 0x588FEC
+	dw_client_state_get_local_player = 0x180
+	dw_entity_list = 0x4DA31EC
+	dw_glow_object_manager = 0x52EB658
+	dw_force_attack = 0x31D3714
+	dw_force_jump = 0x524CFDC
+	m_i_team_num = 0xF4
+	m_i_crosshair_id = 0xB3E8
+	m_i_health = 0x100
+	m_i_glow_index = 0xA438
+	m_f_flags = 0x104
+
+
+class Vars:
+	process = None
+	engine = None
+	client = None
+	player = None
---
+++ main.py
@@ -0,0 +1,46 @@
+import keyboard
+import pymem.process
+from pymem import Pymem
+from Helpers import Offset, Vars
+
+cheat_is_running = True
+
+def main():
+
+    # Load process and client module
+    Vars.process = Pymem('csgo.exe')
+    if not Vars.process:
+        print("Cannot find CSGO")
+        exit(-1)
+    print(f'Process found: {Vars.process.process_id}')
+
+    Vars.engine = pymem.process.module_from_name(Vars.process.process_handle, 'engine.dll').lpBaseOfDll
+    Vars.client = pymem.process.module_from_name(Vars.process.process_handle, 'client.dll').lpBaseOfDll
+
+    if not Vars.engine or not Vars.client:
+        print('Cant find modules')
+        exit(-1)
+    print(f'Engine module: {Vars.engine}, Client module: {Vars.client}')
+
+    client_state = Vars.process.read_int(Vars.engine + Offset.dw_client_state)
+    if not client_state:
+        print('Cant find client state')
+        exit(-1)
+    print(f'Client state {client_state}')
+
+    global cheat_is_running
+    # Run the code for every frame of the game
+    while cheat_is_running:
+
+        # Try to find player entity in memory and run it until player found
+        player_index = Vars.process.read_int(client_state + Offset.dw_client_state_get_local_player)
+        Vars.player = Vars.process.read_int(Vars.client + Offset.dw_entity_list + (player_index * 0x10))
+
+        #TODO: Add handlers
+
+        if keyboard.is_pressed('F8'):
+            cheat_is_running = False
+
+
+if __name__ == '__main__':
+    main()
