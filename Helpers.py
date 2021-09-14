da2be45ac2834199b0676476fad28c2bb76ddd92
Hamed Mahmoudkhani <ainyava@gmail.com>
2021-09-15 00:59:56 +0430

    Game update 2021.09.10

--- Helpers.py
+++ Helpers.py
@@ -1,10 +1,10 @@
 class Offset:
 	dw_client_state = 0x588FEC
 	dw_client_state_get_local_player = 0x180
-	dw_entity_list = 0x4DA542C
-	dw_glow_object_manager = 0x52ED8A8
-	dw_force_attack = 0x31D59B4
-	dw_force_jump = 0x524F21C
+	dw_entity_list = 0x4DA541C
+	dw_glow_object_manager = 0x52ED8B0
+	dw_force_attack = 0x31D59A4
+	dw_force_jump = 0x524F20C
 	m_i_team_num = 0xF4
 	m_i_crosshair_id = 0xB3E8
 	m_i_health = 0x100
